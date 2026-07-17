b8515e065d529726e2503efde7095f34232acfc5
Darío <20240167@ricaldone.edu.sv>
2026-07-17 13:36:32 -0600

    completo

--- src/controllers/wompiController.js
+++ src/controllers/wompiController.js
@@ -22,6 +22,9 @@ wompiController.generarToken = async(req, res) => {
             const error = await response.text();
             return res.status(500).json({error});
         }
+
+        const data = await response.json();
+        return res.status(200).json({data})
     } catch (error) {
     console.log("Error" + error);
     return res.status(500).json({ message: "Internal server error" });
